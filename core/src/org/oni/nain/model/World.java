8e19cf1d658abb28d80cdc52a43580ee654f2159
OniTux <daniel.chesters@gmail.com>
2014-04-19 23:27:28 +0200

    Some change in position logic

--- core/src/org/oni/nain/model/World.java
+++ core/src/org/oni/nain/model/World.java
@@ -12,14 +12,14 @@ import org.oni.nain.model.enumeration.TypeBlock;
 
 public class World {
 
-    private static final int MAX_Y = 50;
-    private static final int MAX_X = 50;
+    private static final int MAX_Y = 2;
+    private static final int MAX_X = 2;
     private List<Block> blocks;
 
     public World() {
         blocks = new ArrayList<>();
-        IntStream.range(-MAX_X, MAX_X).forEach(
-                x -> IntStream.range(-MAX_Y, MAX_Y).forEach(
+        IntStream.rangeClosed(0, MAX_X).forEach(
+                x -> IntStream.rangeClosed(0, MAX_Y).forEach(
                         y -> blocks.add(new Block(x, y, 0, null))));
 
 
