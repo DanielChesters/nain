479b783fcd1be91c0255931c45295fd6326d14aa
OniTux <daniel.chesters@gmail.com>
2014-04-19 18:44:36 +0200

    initial commit

---
+++ desktop/src/org/oni/nain/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.oni.nain.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.oni.nain.NainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NainGame(), config);
+	}
+}
