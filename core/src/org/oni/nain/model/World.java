02a01a0f8481f4de660f28652671c14c5a2425fa
OniTux <daniel.chesters@gmail.com>
2014-04-19 21:14:18 +0200

    Add constants

--- core/src/org/oni/nain/model/World.java
+++ core/src/org/oni/nain/model/World.java
@@ -8,12 +8,14 @@ import org.oni.nain.model.enumeration.TypeBlock;
 
 public class World {
 
+    private static final int MAX_Y = 50;
+    private static final int MAX_X = 50;
     private List<Block> blocks;
 
     public World() {
         blocks = new ArrayList<>();
-        IntStream.range(-50, 50).forEach(
-                x -> IntStream.range(-50, 50).forEach(
+        IntStream.range(-MAX_X, MAX_X).forEach(
+                x -> IntStream.range(-MAX_Y, MAX_Y).forEach(
                         y -> blocks.add(new Block(x, y, 0, TypeBlock.AIR))));
     }
 
