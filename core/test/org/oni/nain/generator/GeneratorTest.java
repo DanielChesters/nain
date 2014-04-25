f5463d9553c601f1b02e3e552b87bd3da547cd3f
Daniel Chesters <daniel.chesters@gmail.com>
2014-04-25 22:22:22 +0200

    Improve test

--- core/test/org/oni/nain/generator/GeneratorTest.java
+++ core/test/org/oni/nain/generator/GeneratorTest.java
@@ -3,6 +3,7 @@ package org.oni.nain.generator;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -11,15 +12,38 @@ import org.oni.nain.model.enumeration.Biome;
 
 @RunWith(JUnit4.class)
 public class GeneratorTest {
+
+    private List<Block> blocks;
+
+    @Before
+    public void setup() {
+        blocks = Generator.INSTANCE.generateBlocks(10);
+    }
+
     @Test
-    public void generateBlockTest() {
-        List<Block> blocks = Generator.INSTANCE.generateBlocks(10);
+    public void generateBlockIsNotEmptyTest() {
         Assert.assertNotNull(blocks);
         Assert.assertFalse(blocks.isEmpty());
+    }
+
+    @Test
+    public void generateBlockSize() {
         Assert.assertEquals(100, blocks.size());
-        Assert.assertTrue(blocks.stream().anyMatch(b -> b.getBiome() == Biome.AIR));
-        Assert.assertTrue(blocks.stream().anyMatch(b -> b.getBiome() == Biome.WATER));
-        Assert.assertTrue(blocks.stream().anyMatch(b -> b.getBiome() == Biome.DIRT));
+    }
+
+    @Test
+    public void generateBlockHaveWater() {
+        Assert.assertTrue(blocks.parallelStream().anyMatch(b -> b.getBiome() == Biome.WATER));
+    }
+
+    @Test
+    public void generateBlockHaveAir() {
+        Assert.assertTrue(blocks.parallelStream().anyMatch(b -> b.getBiome() == Biome.AIR));
+    }
+
+    @Test
+    public void generateBlockHaveDirt() {
+        Assert.assertTrue(blocks.parallelStream().anyMatch(b -> b.getBiome() == Biome.DIRT));
     }
 
 }
